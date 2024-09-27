940da80dea8d67a2f04e86778b2e3081270b8871
André Coêlho <alcbcoelho@gmail.com>
2024-09-27 19:29:33 -0300

    Estilos convertidos para styled-components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,54 @@
-import styles from './Hero.module.css'
+import styled from 'styled-components'
+// import styles from './Hero.module.css'
+
+const HeroStyle = styled.form`
+  height: 360px;
+  width: 100%;
+  background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
+  background-size: cover;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    height: auto;
+    padding: 24px 0;
+  }
+
+  &::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--cor-principal);
+    content: '';
+    opacity: 0.7;
+  }
+
+  div {
+    position: relative;
+    color: #eee;
+  }
+
+  .heroTitle {
+    font-family: Gloock, serif;
+    font-size: 48px;
+
+    @media (max-width: 768px) {
+      font-size: 32px;
+    }
+  }
+`
 
 const Hero = () => (
-  <form className={styles.form}>
+  <HeroStyle>
     <div className="container">
-      <h2 className={styles.heroTitle}>
+      <h2 className="heroTitle">
         As melhores vagas para tecnologia, design e artes visuais.
       </h2>
     </div>
-  </form>
+  </HeroStyle>
 )
 
 export default Hero
--- src/components/Vaga/index.tsx
+++ src/components/Vaga/index.tsx
@@ -1,4 +1,47 @@
-import styles from './Vaga.module.css'
+import styled from 'styled-components'
+
+const VagaStyle = styled.li`
+  border: 1px solid var(--cor-principal);
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  padding: 16px;
+  transition: all ease 0.3s;
+  border-radius: 8px;
+
+  .vagaTitulo {
+    font-weight: bold;
+    margin-bottom: 16px;
+  }
+
+  .vagaLink {
+    border-color: var(--cor-secundaria);
+    background-color: var(--cor-principal);
+    color: var(--cor-secundaria);
+    display: inline-block;
+    padding: 8px 16px;
+    text-decoration: none;
+    margin-top: 16px;
+    font-weight: bold;
+    font-size: 14px;
+    border-radius: 8px;
+    text-align: center;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+
+  &:hover {
+    background-color: var(--cor-principal);
+    color: var(--cor-secundaria);
+  }
+
+  &:hover a {
+    border-color: var(--cor-principal);
+    background-color: var(--cor-secundaria);
+    color: var(--cor-principal);
+  }
+`
 
 type Props = {
   titulo: string
@@ -11,8 +54,8 @@ type Props = {
 }
 
 const Vaga = (props: Props) => (
-  <li className={styles.vaga}>
-    <h3 className={styles.vagaTitulo}>{props.titulo}</h3>
+  <VagaStyle>
+    <h3 className="vagaTitulo">{props.titulo}</h3>
     <ul>
       <li>Localizacao: {props.localizacao}</li>
       <li>Senioridade: {props.nivel}</li>
@@ -22,10 +65,10 @@ const Vaga = (props: Props) => (
       </li>
       <li>Requisitos: {props.requisitos.join(', ')}</li>
     </ul>
-    <a className={styles.vagaLink} href="#">
+    <a className="vagaLink" href="#">
       Ver detalhes e candidatar-se
     </a>
-  </li>
+  </VagaStyle>
 )
 
 export default Vaga
